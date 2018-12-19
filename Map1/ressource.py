7451075be8b53d7127b12032de99a9769c2dcdc1
leofarhi <37966380+leofarhi@users.noreply.github.com>
2018-12-19 11:04:42 +0100

    Update ressource.py

--- Map1/ressource.py
+++ Map1/ressource.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import urllib3
 import requests
 import texture
+import entité
 rep=os.getcwd()+"//lib//"
 sys.path.insert(0, rep)
 
@@ -15,3 +16,14 @@ with open(rep+"entité"+".txt", "r") as fic:
     i=fic.readline()
     eval(compile("entité_element="+str(i), '<string>', 'exec'))
     fic.close()
+
+def écrit(code,name):
+    with open(rep+"//"+str(name)+".py", "w",encoding='utf-8', errors='ignore') as fic:
+        fic.write(code)
+        fic.close()
+def game_refress(liste):
+
+    for i in liste:
+        eval("surface.blit("+str(i[0])+", ("+str(i[1]+x)+","+str(i[2]+y)+"))")
+        
+game_refress(entité_element)
